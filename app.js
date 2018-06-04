159d813fa8e5a20b9f491442c4da4a0184792c9d
maxvidrine <max.vidrine@gmail.com>
2018-06-04 13:31:00 -0700

    created a basic loop of event handlers for questions

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+let scorecard = [];
+
 let quiz = [
 	{
-		qtext: "What products will Prop E not regulate?",
+		qtext: "What products will Prop E not regulate",
 		options: [
 			{
-				otext: "Cigarettes and cigars",
+				otext: "Normal cigarettes and cigars",
 				isCorrect: true
 			},
 			{
-				otext: "Cigarello wrappers",
+				otext: "Grape flavored cigarello wrappers",
 				isCorrect: false
 			},
 			{
@@ -16,4 +18,50 @@ let quiz = [
 			},
 		]
 	}
-]
+];
+
+const main = $('main')[0].html();
+
+const loadFeedback = (oindex) => {
+	main = '';
+	let rightOrWrong = ''
+	if (options[oindex].isCorrect) {
+		rightOrWrong = 'right';
+    } else {rightOrWrong = 'wrong';}
+    let points = scorecard.filter(correct => correct).length;
+	main.append(`<h1>You got it ${rightOrWrong}!</h1><div class="next"><a href="#">Next Question</a></div><div class="points">You've earned ${points} points so far.</div>`);
+	main.firstChild().on('click', (e) => {
+		e.preventDefault();
+		qindex++;
+		if (qindex > 3) {
+			loadScore();
+		}
+		loadQuestion(quizObject, qindex);
+	});
+}
+
+const loadQuestion = (quizObject, index) => {
+	let qindex = index;
+	let question = quizObject[index];
+	let options = question.options;
+	const htmlTemplate = `
+	    <div class="question">
+	    	<div class="top"></div>
+            <div class="qtext">${question.qtext}</div>
+            <div class="option"><a href="#" class="${options[0].isCorrect}">${options[0].otext}</a></div>
+            <div class="option"><a href="#" class="${options[1].isCorrect}">${options[1].otext}</a></div>
+            <div class="option"><a href="#" class="${options[2].isCorrect}">${options[2].otext}</a></div>
+        </div>
+    	`;
+    $('main').append(htmlTemplate);
+    $('option').each(oindex => {
+    	$(this).on('click', (e, oindex) => {
+    		e.preventDefault();
+    		loadFeedback(oindex);
+    	})
+    });
+}
+
+const loadScore = () => {}
+
+loadQuestion(quiz, 0);
