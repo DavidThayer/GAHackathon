75647405b752acfc5b0ee3a1d2aaf77f3aedd5ae
maxvidrine <max.vidrine@gmail.com>
2018-06-04 12:12:02 -0700

    created a skeleton quiz object

---
+++ app.js
@@ -0,0 +1,19 @@
+let quiz = [
+	{
+		qtext: "What products will Prop E not regulate?",
+		options: [
+			{
+				otext: "Cigarettes and cigars",
+				isCorrect: true
+			},
+			{
+				otext: "Cigarello wrappers",
+				isCorrect: false
+			},
+			{
+				otext: "Vape juice",
+				isCorrect: false
+			},
+		]
+	}
+]
